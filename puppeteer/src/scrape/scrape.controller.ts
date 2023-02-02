5c65bd3841a885df5c1e1badf53ffdf994191f83
motoure <motoure@student.42.fr>
2023-02-02 21:11:51 +0100

    returns positive / negative / neutral predictions from given url paragraphs content

--- puppeteer/src/scrape/scrape.controller.ts
+++ puppeteer/src/scrape/scrape.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Post, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Request, response } from 'express';
 import { BlobOptions } from 'buffer';
 
 interface paragraphs {
@@ -19,13 +19,28 @@ interface scrapeInfo {
 }
 
 const puppeteer = require('puppeteer');
+const cohere = require('cohere-ai');
+cohere.init('lJR6qlpmD2AzOAjbX36VM9FgcGTnWvMh8Uj6oGdg');
+
+interface exampleTemplate {
+  text: string;
+  label: string;
+}
+const Example = (text, label): exampleTemplate => {
+  let ret: exampleTemplate = {
+    text,
+    label,
+  };
+  return ret;
+};
 
 @Controller('scrape')
 export class ScrapeController {
   @Get('fromUrl')
-  url(@Req() req: Request, @Body() dto: scrapeInfo): string {
+  async url(@Req() req: Request, @Body() dto: scrapeInfo) {
     console.log(dto.url);
-    let paragraphs = undefined;
+    let paragraphs: string[] = [];
+    let ret = {};
     async function run() {
       const browser = await puppeteer.launch({
         headless: true,
@@ -36,20 +51,58 @@ export class ScrapeController {
       await page.goto(dto.url, {
         waitUntil: 'domcontentloaded',
       });
-      // wait for paragraphs
-      await page.waitForSelector('p', { timeout: 5_000 }).then(async () => {
-        let _paragraphs = await page.$$('p');
-        for (let i = 0; i < _paragraphs.length; i++) {
-          console.log(_paragraphs[i]);
-        }
-        console.log(_paragraphs.length);
-        // console.log(_paragraphs[0].evaluate((node) => node.innerText));
+      console.log(await page.content());
+      const pElements = await page.$$eval('p', (p) =>
+        p.map((p) => p.innerText),
+      );
+      // remove empty string
+      pElements.map((el) => {
+        el !== '' && paragraphs.push(el);
       });
-      // console.log(await page.content());
+      console.log(paragraphs);
+      // close puppeteer
       await page.close();
       await browser.close();
+
+      const response = await cohere.classify({
+        inputs: paragraphs,
+        examples: [
+          Example(
+            "I've still got my Christmas tree up - it's so cosy and too much effort to take down",
+            'positive',
+          ),
+          Example(
+            'How black barbers in London are giving poor mental health the chop',
+            'positive',
+          ),
+          Example(
+            'European equities are holding steady in a solid range with Dax and FTSE both stalling at resistance.',
+            'neutral',
+          ),
+          Example(
+            'This morning saw the release of Nationwide housing price data out of the UK ',
+            'neutral',
+          ),
+          Example(
+            'For six days, a blast of polar air from Canada has been wrecking holiday travel plans as it lumbers across the country, leaving power outages, canceled flights and dangerous roads in its wake.',
+            'negative',
+          ),
+          Example(
+            'Western New York saw days of sustained blizzard conditions',
+            'negative',
+          ),
+        ],
+      });
+      console.log(JSON.stringify(response));
+      let [positive, negative, neutral] = [0, 0, 0];
+      const iter = (obj) => {
+        Object.keys(obj).forEach((key) => {
+          console.log(key);
+          if (typeof obj[key] === 'object' && obj[key] !== null) iter(obj[key]);
+        });
+      };
+      return response;
     }
-    run();
-    return 'okok';
+    return await run();
   }
 }
