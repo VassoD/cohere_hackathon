f84946e36a5055bb5d6cd70e3f1d4fb72c60a0b0
Vasiliki Doropoulou <vasiliki@ublo.immo>
2023-07-30 00:38:22 +0200

    fixes

--- static/main.js
+++ static/main.js
@@ -1,34 +1,45 @@
-$(document).ready(function() {
-  $('#search_query').focus(function() {
-    if (this.value == 'Generate a News Headline') {
-      this.value = '';
+$(document).ready(function () {
+  $("#search_query").focus(function () {
+    if (this.value == "Generate a News Headline") {
+      this.value = "";
     }
   });
 
-  $('#search_query').blur(function() {
-    if (this.value == '') {
-      this.value = 'Generate a News Headline';
+  $("#search_query").blur(function () {
+    if (this.value == "") {
+      this.value = "Generate a News Headline";
     }
   });
 });
 
 $(document).ready(function () {
   $("#search_query").on("input", function () {
-      if ($(this).val().length > 0) {
-          $("#search-submit").removeAttr("disabled").css("background-color", "#B08383");
-          $("#search-submit").hover(function () {
-              $(this).css("background-color", "#ccc");
-          }, function () {  // on mouseout
-              $(this).css("background-color", "#B08383");
-          });
-      } else {
-          $("#search-submit").attr("disabled", "disabled").css("background-color", "#ccc");
-      }
+    if ($(this).val().length > 0) {
+      $("#search-submit")
+        .removeAttr("disabled")
+        .css("background-color", "#B08383");
+      $("#search-submit").hover(
+        function () {
+          $(this).css("background-color", "#ccc");
+        },
+        function () {
+          // on mouseout
+          $(this).css("background-color", "#B08383");
+        }
+      );
+    } else {
+      $("#search-submit")
+        .attr("disabled", "disabled")
+        .css("background-color", "#ccc");
+    }
   });
 });
 
-$(document).ready(function(){
-  $(".navbar-toggler").click(function(){
+$(document).ready(function () {
+  $(".navbar-toggler").click(function () {
     $(".navbar-collapse").slideToggle();
   });
 });
+function startSpinner() {
+  document.getElementById("spinner").style.display = "block";
+}
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["@static/*.{js,css}", "@templates/*.{html}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
